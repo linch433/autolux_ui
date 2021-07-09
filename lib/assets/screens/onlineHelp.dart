832b08f9e2326eceea170b99da3d8498b70c0996
linch433 <linch433@gmail.com>
2021-07-09 15:43:53 +0300

    Change onlineHelp screen

--- lib/assets/screens/onlineHelp.dart
+++ lib/assets/screens/onlineHelp.dart
@@ -5,30 +5,65 @@ class onlineHelp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-        title: Center(
-          child: Text(
-            'Лінія підтримки',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                width: size.width,
+                height: size.height * .125,
+                color: Colors.lightBlue,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SafeArea(
+                      child: Text(
+                        'Лінія підтримки',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 20),
+                      ),
+                    ),
+                    SafeArea(
+                      child: IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Icon(Icons.close),
+                        color: Colors.white54,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ),
-        leading: Icon(null),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(Icons.close),
-            color: Colors.white54,
+          Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Введите сообщение...',
+                        icon: Icon(Icons.alternate_email_outlined),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+            ],
           )
         ],
-      ),
-      body: Center(
-        child: Text(
-          'Мы готовы вам помочь',
-        ),
       ),
     );
   }
